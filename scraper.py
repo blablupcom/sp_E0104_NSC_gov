c46c200cb4fec4552409b4c1e11e0be2ed37a9c9
blablupcom <o.volodin@yahoo.com>
2016-04-11 21:57:01 +0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -16,7 +16,7 @@
 #
 # # An arbitrary query against the database
 # scraperwiki.sql.select("* from data where 'name'='peter'")
-
+print 'http://example.com'
 # You don't have to do things with the ScraperWiki and lxml libraries.
 # You can use whatever libraries you want: https://morph.io/documentation/python
 # All that matters is that your final data is written to an SQLite database
